7ab702985b682aadf8402bd068247f65aec602c5
d-tynes <dahliatynes@icloud.com>
2025-01-10 14:04:31 -0800

    Add files via upload

---
+++ Fraction/FDriver.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+class FDriver{
+    
+    public static void main (String[]args){
+        
+       Fraction half = new Fraction(1,2);
+       Fraction third = new Fraction(1,3);
+       
+       System.out.println(half.multiply(half,third));
+       System.out.println(half.divide(half,third));
+       System.out.println(half.add(half,third));
+       System.out.println(half.subtract(half,third));
+       
+        
+    }
+}
---
+++ Fraction/Fraction.java
@@ -0,0 +1,116 @@
+/* Dahlia Tynes
+ * 7 January 2025
+ * APCSA  */
+
+public class Fraction{
+    private int numerator;
+    private int denominator;
+    
+    public Fraction(){
+        numerator =1;
+        denominator =2;
+    }
+    
+    public Fraction(int n, int d){
+        numerator=n;
+        if(d==0){
+            System.out.println("ERROR: denominator cannot be zero");
+            d=1;
+        }else denominator=d;
+        
+    }
+    
+    public Fraction(String str){
+        String s=str;
+      String[] temp= s.split(",");
+      numerator = Integer.parseInt(temp[0]);
+      denominator = Integer.parseInt(temp[1]);
+      
+      if(denominator==0) denominator=1;
+      
+    }
+    
+    public Fraction(Fraction f){
+        numerator=f.numerator;
+        denominator=f.denominator;
+    }
+    
+    public int getNum(){
+        return numerator;
+    }
+    
+    public int getDenom(){
+        return denominator;
+    }
+    
+    public double toDouble(){
+        return (double) numerator/denominator;
+    }
+    
+    public String toString(){
+        return (numerator+"/"+denominator);
+    }
+    
+    public void reduce(){
+        int factor = GCF(Math.abs(numerator), (Math.abs(denominator)));
+        numerator/= factor;
+        denominator /=factor;
+    }
+    
+    public void setNum(int replacement){
+        numerator=replacement;
+    }
+    
+    public void setDenom(int replacement){
+        denominator=replacement;
+    }
+    
+    
+    private int GCF(int a, int b){
+         
+        if(a==b) return a;
+        if(a>b) return GCF(a-b,a);
+        else return GCF(a,b-a);
+        
+    }
+    
+    public static Fraction multiply(Fraction a, Fraction b){
+        int newNum=a.numerator*b.numerator;
+        int newDenom=a.denominator*b.denominator;
+        
+        Fraction newFraction = new Fraction(newNum,newDenom);
+        return newFraction;
+    }
+    
+    public static Fraction divide(Fraction a, Fraction b){
+        int newNum=a.numerator*b.denominator;
+        int newDenom=a.denominator*b.numerator;
+        
+        Fraction newFraction = new Fraction(newNum,newDenom);
+        return newFraction;
+    }
+    
+    public static Fraction add(Fraction a, Fraction b){
+        int newDenom=a.denominator*b.denominator;
+        int newNumerator=(a.numerator*b.denominator + b.numerator*a.denominator);
+    
+        Fraction newFraction=new Fraction(newNumerator,newDenom);
+        return newFraction;
+        
+    }
+    
+    public static Fraction subtract(Fraction a, Fraction b){
+        int newDenom=a.denominator*b.denominator;
+        int newNumerator=(a.numerator*b.denominator - b.numerator*a.denominator);
+    
+        Fraction newFraction=new Fraction(newNumerator,newDenom);
+        return newFraction;
+        
+    }
+    }
+    
+    
+    
+    
+    
+    
